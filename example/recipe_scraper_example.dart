a57379cb4bef5a0eefb24027b9477242f22bee57
Malte2036 <malte2036@gmail.com>
2023-10-27 23:35:14 +0200

    :tada: Begin project

---
+++ example/recipe_scraper_example.dart
@@ -0,0 +1,6 @@
+import 'package:recipe_scraper/recipe_scraper.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/recipe_scraper_test.dart
@@ -0,0 +1,16 @@
+import 'package:recipe_scraper/recipe_scraper.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
